113154f58f0dfe4e3f7fbf45fa2f6cda03532557
Rituraj Singh <riturajsingh1173@gmail.com>
2026-06-24 16:01:20 +0530

    Added responsive navbar

--- script.js
+++ script.js
@@ -45,3 +45,9 @@ function typeEffect() {
 }
 
 typeEffect();
+const menuBtn = document.querySelector(".menu-btn");
+const navMenu = document.querySelector("nav ul");
+
+menuBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
