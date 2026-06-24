0f375476c749aae1ef0a20aec4162fd4f57ee762
Rituraj Singh <riturajsingh1173@gmail.com>
2026-06-24 15:43:22 +0530

    Update

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Portfolio Loaded Successfully");
+const roles = [
+    "Frontend Developer",
+    "Web Developer",
+    "CSE Student"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let currentRole = "";
+let isDeleting = false;
+
+function typeEffect() {
+
+    currentRole = roles[roleIndex];
+
+    if (!isDeleting) {
+        document.getElementById("typing").textContent =
+            currentRole.substring(0, charIndex + 1);
+
+        charIndex++;
+
+        if (charIndex === currentRole.length) {
+            isDeleting = true;
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+    }
+    else {
+        document.getElementById("typing").textContent =
+            currentRole.substring(0, charIndex - 1);
+
+        charIndex--;
+
+        if (charIndex === 0) {
+            isDeleting = false;
+            roleIndex++;
+
+            if (roleIndex === roles.length) {
+                roleIndex = 0;
+            }
+        }
+    }
+
+    setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
